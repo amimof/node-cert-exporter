3a9cc83f766eebb3f56fe6d5f29e28b505f58939
Amir Mofasser <amir.mofasser@gmail.com>
2019-04-23 11:11:07 +0200

    Added unit tests for `Exporter`

--- pkg/exporter/exporter_test.go
+++ pkg/exporter/exporter_test.go
@@ -1,1 +1,34 @@
 package exporter
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	e := New()
+	// Roots should be empty
+	if len(e.roots) != 0 {
+		t.Fatalf("roots expected to be %d, got %d", 0, len(e.roots))
+	}
+	// Set new root
+	roots := []string{"/etc/ssl","/etc/kubernetes","/etc/origin"}
+	e.SetRoots(roots)
+	if len(e.roots) != 3 {
+		t.Fatalf("roots expected to be %d, got %d", len(roots), len(e.roots))
+	}
+}
+
+func TestIsCertFile(t *testing.T) {
+	certs := []string{"/etc/ssl/ca.pem", "~/.certs/mydomain.crt", "custom-certificate.cert", "./current/dir/server.cer"}
+	for _, cert := range certs {
+		if !isCertFile(cert) {
+			t.Fatalf("Path %s expected to return true, got false", cert)
+		}
+	}
+	notCerts := []string{"/etc/ssl/ca.pem/dir", "~/.certs/mydomain.crt.txt", "custom-certificate", "./current/dir/server.cer-cer"}
+	for _, cert := range notCerts {
+		if isCertFile(cert) {
+			t.Fatalf("Path %s expected to return false, got true", cert)
+		}
+	}
+}
