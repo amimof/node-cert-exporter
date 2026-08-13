3527b1e9b5d561543e38f0ccabeb60d02622d1ec
Frederiko Costa <frederiko@gmail.com>
2026-08-13 09:36:47 -0700

    Upgrade Golang 1.26.5 with libraries upgrade
    Signed-off-by: Frederiko Costa <frederiko.costa@workday.com>

--- cmd/node-cert-exporter/main.go
+++ cmd/node-cert-exporter/main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/amimof/node-cert-exporter/pkg/exporter"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors/version"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/common/version"
 	"github.com/spf13/pflag"
 )
 
